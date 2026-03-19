7d960b8198ea3b5f1ea522ace8b8c69f33bbae92
Liarlindofo <liar.contato@outlook.com>
2026-03-19 20:03:53 -0300

    fix(cmv): remove exports de ChatPanel, ChartsPanel e CMVTable deletados

--- app/cmv/components/index.ts
+++ app/cmv/components/index.ts
@@ -1,7 +1,7 @@
 export { CMVDashboard } from './CMVDashboard';
 export { StoreTab } from './StoreTab';
 export { ComparisonTab } from './ComparisonTab';
-export { ChatPanel } from './ChatPanel';
-export { ChartsPanel } from './ChartsPanel';
-export { CMVTable } from './CMVTable';
 export { MetricCards } from './MetricCards';
+export { PizzaCard } from './PizzaCard';
+export { PizzaModal } from './PizzaModal';
+export { AddProductModal } from './AddProductModal';
